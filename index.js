4c202404c4b44fb5fa346eb02e21a71136f08f6e
Carson Buckley <45154466+CarsonBuckley@users.noreply.github.com>
2022-06-29 15:06:10 -0600

    added css and js

---
+++ index.js
@@ -0,0 +1,8 @@
+const buyButton = document.querySelector('#buy')
+
+const message = () => {
+    alert('Purchase made successfully')
+
+}
+
+buyButton.addEventListener('click', message)
--- server/index.js
+++ server/index.js
@@ -7,13 +7,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-// app.get('/css', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../styles.css'))
-// })
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+})
 
-// app.get('/js', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../index.js'))
-// })
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.js'))
+})
 
 const port = process.env.PORT || 4005
 
